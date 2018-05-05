4ede71b67a8abce4118fae8367814437637f5042
advaitambeskar <ambeskaradvait@gmail.com>
2018-05-05 02:31:33 -0400

    Added Datatype coersion knowledge

--- app.js
+++ app.js
@@ -6,3 +6,4 @@ let x = 7;
 let y = 3;
 let z = x + y;
 console.log('Answer: ' + z);
+
---
+++ datatypes.js
@@ -0,0 +1,18 @@
+let x = 7; // numeric values
+console.log(typeof x);
+
+let y = true; //true or false
+console.log(typeof y);
+
+let z = 'hello world' //anything instead of single quotes
+console.log(typeof z);
+
+let a = 7;
+let b = '6';
+let c = a + b;
+console.log('Answer : ' + c); //output = 76
+
+//type conversion
+b = parseInt(b, 10);
+c = a + b;
+console.log('Answer : ' + c);

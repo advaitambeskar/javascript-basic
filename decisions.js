6806f250686219c5253e1977aba590639a85cd53
advaitambeskar <ambeskaradvait@gmail.com>
2018-05-05 07:38:12 -0400

    functions and decision statements

---
+++ decisions.js
@@ -0,0 +1,10 @@
+var count = 3;
+do{
+if (/* expression is true */ count == 4){
+    console.log("YOU ARE AWESOME!")
+    ++ count;
+} else{
+    console.log("OKAY?!");
+    ++count;
+}
+}while(count < 10);

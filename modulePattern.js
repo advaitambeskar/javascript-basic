58dbf957d5c2f466493fed99317663dff55fd873
advaitambeskar <ambeskaradvait@gmail.com>
2018-05-15 20:32:16 +0530

    module pattern

--- modulePattern.js
+++ modulePattern.js
@@ -9,26 +9,36 @@ var counter = (function() {
         console.log(message + ' --- ' + count);
     }
 
+    function get(){
+        return count;
+    }
+    function set(value){
+        count = value;
+    }
+    
+    function increment(){
+        count += 1;
+        print("After Increment ");
+    }
+
+    function reset(){
+        count = 0;
+        print("After Reset ");
+    }
 
     //return an object full of properties
     return{
-        get: function(){ return count; },
-        set: function(value){ count = value;},
-        increment: function(){
-            count += 1;
-            print("After Increment ");
-        },
-        reset: function(){
-            count = 0;
-            print("After Reset ");
-        }
+        getValue: get(),
+        setValue: function(value){set(value)},
+        incrementValue: increment(),
+        resetValue: reset()        
     }
 })();
 //console.log(counter);
-console.log(counter.count);
-counter.increment();
-counter.increment();
-console.log(counter.get());
-counter.set(10);
-counter.increment();
-counter.reset();
+//console.log(counter.count);
+counter.incrementValue;
+counter.incrementValue;
+console.log(counter.getValue);
+counter.setValue(10);
+counter.incrementValue;
+counter.resetValue;

ee3fbba3dcbe2da68f46b0beca6a2360f10ce62d
advaitambeskar <ambeskaradvait@gmail.com>
2018-05-18 10:16:53 -0400

    Completed destructuring

--- destructuring.js
+++ destructuring.js
@@ -3,10 +3,32 @@
 let a, b, c, d, e;
 
 let names = ['david', 'eddie', 'alex', 'micheal', 'bob'];
-
+/*
 [a, b, c, d, e] = names;
 
 console.log(c);
 console.log(b);
 console.log(a);
 console.log(e);
+*/
+/*
+let others;
+[a, b, ...others] = names;
+
+console.log(a);
+console.log(b);
+console.log(others);
+
+*/
+
+//working with destructuring and object
+
+let year, model;
+({year, model} = {
+    make: 'bmw',
+    model: '745li',
+    value: 5000,
+    year: 2010
+});
+console.log(year);
+console.log(model);

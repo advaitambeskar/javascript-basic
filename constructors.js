61fa75b916762bca9c1581bb65b042d4acb839fa
advaitambeskar <ambeskaradvait@gmail.com>
2018-05-21 10:50:34 -0400

    Added constructors and objectstudies

---
+++ constructors.js
@@ -0,0 +1,21 @@
+/* 
+function Car (make, model, year){
+    this.make = make;
+    this.model = model;
+    this.year = year;
+}
+
+let myCar = new Car('bmw', '745li', 2010); //new keyword is the constructor call
+
+console.log(myCar);
+console.log(myCar.make);
+ */
+
+ function MyFunction(){
+     console.log('I am a simple function');
+ }
+ let myFunct = new MyFunction();
+
+ console.log(typeof myFunct);
+
+ //myFunct is just an object!
